3ed6be8f9e28d19ebf4a3ea631dc61d4e2254251
João Henrique <joaoenrique.san@gmail.com>
2023-09-24 19:51:12 -0300

    feat: create a projects page with transitions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # My personal spot on the World Wide Web ;) 
 
-*built with [astro](astro.build), [tailwind](tailwindcss.com) and lots of future funk.*
+*built with [astro](astro.build), [astro image tools](https://astro-imagetools-docs.vercel.app/), [lottiefiles](https://lottiefiles.com/), [tailwind](tailwindcss.com) and lots of future funk.*
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lottie()],
+  integrations: [tailwind(), lottie(), astroImageTools],
 });
