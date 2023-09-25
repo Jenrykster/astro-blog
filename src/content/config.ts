e7455055eb4bfe8b9a8868fd0f266b5547db455b
João Henrique <joaoenrique.san@gmail.com>
2023-09-24 23:01:06 -0300

    feat: add a technology list to projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
 import { defineCollection, z } from "astro:content";
-
-const technologies = z.enum([
+export const technologies = [
   "typescript",
   "react",
   "next",
   "nest",
   "node",
   "other",
-]);
+] as const;
+const technologiesEnum = z.enum(technologies);
 
 const projectCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    technologies: z.array(technologies),
+    technologies: z.array(technologiesEnum),
     image: z.string().optional(),
     repo: z.string().optional(),
     link: z.string().optional(),
