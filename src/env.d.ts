ee837ec17d11f27ef465e8430279790b8592a7a8
João Henrique <joaoenrique.san@gmail.com>
2023-10-05 23:28:10 -0300

    feat: add an animated icon for the theme toggler

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
+/// <reference types="astro-integration-lottie/env" />
 declare global {
-  function loadLottie(): Promise<void>;
+  function loadLottie(id?: string): Promise<void>;
 }
 
 export {};
